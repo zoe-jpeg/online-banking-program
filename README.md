075bb41d597b4e7528b35cbfa3bac292f51d7c7d
Zoe Bryant <thezoebryant@gmail.com>
2025-04-07 20:00:56 -0500

    connected to mysql database, added new files to do so. updated read.me file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # online-banking-program
+
+Online Banking Program - Elite 102 Project
+
+Objective: Users need to have an account number and PIN to identify themselves as owners of an account. Once users get into the system they will have standard options: check balance, deposit, and withdraw. Additionally, a new user or bank administrator can also create a new account, close account, and modify an account (such as edit name, PIN, or any other personal identification required to open an account).
---
+++ new.py
@@ -0,0 +1,28 @@
+"""import mysql.connector
+
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+c.execute('SELECT * FROM student')
+print(c.fetchall())
+conn.close()"""
+
+import os
+import mysql.connector
+
+os.system("clear")
+
+connection = mysql.connector.connect(user = "root", database = "example", password = "Zb01101944!?*")
+
+cursor = connection.cursor()
+
+testQuery = ('SELECT * FROM student')
+
+cursor.execute(testQuery)
+
+for item in cursor:
+
+    print(item)
+
+cursor.close()
+
+connection.close()
